6601de3fdd924a441ed61e8ebdd9a07012e266b1
syed-majidhassan <95635018+syed-majidhassan@users.noreply.github.com>
2024-11-14 14:27:07 +0100

    Create MajidTest.groovy

---
+++ MajidTest.groovy
@@ -0,0 +1,21 @@
+import com.exalate.basic.domain.hubobject.v1.BasicHubIssue
+
+public class MajidTest{
+  public MajidTest(){
+  }
+  public def mySync(BasicHubIssue issue, BasicHubIssue replica){
+    replica.key            = issue.key
+    replica.type           = issue.type
+    replica.assignee       = issue.assignee
+    replica.reporter       = issue.reporter
+    replica.summary        = issue.summary
+    replica.description    = issue.description
+    replica.labels         = issue.labels
+    replica.comments       = issue.comments
+    replica.resolution     = issue.resolution
+    replica.status         = issue.statusd
+    replica.priority       = issue.priority
+    replica.attachments    = issue.attachments
+
+  }
+}
