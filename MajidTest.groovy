e2d3e52d1f82c2c0659880e7134ec4ee26d00cd2
syed-majidhassan <95635018+syed-majidhassan@users.noreply.github.com>
2024-11-15 11:34:48 +0100

    Update MajidTest.groovy

--- MajidTest.groovy
+++ MajidTest.groovy
@@ -16,7 +16,7 @@ public class MajidTest{
     replica.status         = issue.statusd
     replica.priority       = issue.priority
    // replica.attachments    = issue.attachments
-    issue.customFields."Target Issue Key" = replica.customFields."Target Issue Key"
+    replica.customFields."Target Issue Key" = issue.customFields."Target Issue Key"
 
   }
 }
